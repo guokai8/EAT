f0767cca2470a642ed473f48b04665e9a5a52b7d
bioguo <guokai8@gmail.com>
2021-06-18 15:16:45 -0500

    Update run_EAT.r

--- EAT/RNA-seq/run_EAT.r
+++ EAT/RNA-seq/run_EAT.r
@@ -1,6 +1,6 @@
 library(DESeq2)
 library(ggplot2)
-group<-read.delim("/Users/bioguo/Desktop/EAT_result/EAT_group_cluster.txt",sep="\t",row.names=1)
+group<-read.delim("EAT_group_cluster.txt",sep="\t",row.names=1)
 df<-read.delim("counts.txt",sep="\t",skip=1,row.names=1)
 colnames(df)<-sub('\\_.*.bam','',colnames(df))
 colnames(df)<-sub('X','',colnames(df))
@@ -13,9 +13,14 @@ percentVar <- round(100 * attr(pca, "percentVar"))
 pca$sample<-group$Group
 ggplot(pca,aes(PC1,PC2,color=condition,shape=sample))+geom_point(size=4)+xlab(paste0("PC1: ",percentVar[1],"%"))+ylab(paste("PC2: ",percentVar[2],"%"))
 ggsave(file="EAT_new.pdf")
-#check the housingkeeping genes expression:Ywhaz
-x<-barplot(as.numeric(assay(rld)["ENSRNOG00000008195",]),col=as.factor(condition))
-axis(1,at=x,labels=colnames(dd),las=2)
-dev.print(pdf,file="EAT_Ywhaz.pdf")
-x<-barplot(as.numeric(dd["ENSRNOG00000008195",]),col=as.factor(condition))
-axis(1,at=x,labels=colnames(dd),las=2)
+######
+resbo<-results(dds,contrast = c("condition","Boison_Group1","Boison_Group2"))
+reslu<-results(dds,contrast = c("condition","Lubin_Group2","Lubin_Group1"))
+resru<-results(dds,contrast = c("condition","Ruskin_Group2","Ruskin_Group1"))
+#####
+library(richR)
+rtgo<-buildAnnot(species="rat",keytype="SYMBOL",anntype="GO")
+rtko<-buildAnnot(species="rat",keytype="SYMBOL",anntype="KEGG")
+######
+boko<-richKEGG(rownames(subset(resbo,padj<0.05)),rtko)
+bogo<-richGO(rownames(subset(resbo,padj<0.05)),rtgo)
