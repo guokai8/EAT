1f2d588bbcef272c2b7b2a1740593512158d9329
bioguo <guokai8@gmail.com>
2021-06-21 09:08:30 -0500

    Update run_PCA.R

--- EAT/RRBS/run_PCA.R
+++ EAT/RRBS/run_PCA.R
@@ -13,10 +13,11 @@ x<-as.numeric(x)
 myobj<-processBismarkAln(location=filenames,sample.id=sample.ids,save.folder=NULL,save.context=NULL,read.context="CpG",nolap=FALSE,phred64=FALSE,treatment=x,assembly="rn6")
 mymeth<-unite(myobj, destrand=FALSE)
 per<-percMethylation(mymeth)
+####use all CpG sites or use top 10
 p<-prcomp(t(per))
+explain <- p$sdev^2/sum(p$sdev^2)
 pca<-as.data.frame(p$x)
 library(ggplot2)
 pca$group<-sub('\\..*','',rownames(pca))
-ggplot(pca,aes(PC1,PC2,color=group))+geom_point()+theme_light()+xlab('')
-
+ggplot(pca,aes(PC1,PC2,color=group))+geom_point()+theme_light()+xlab(paste0("PC1: ("explain[1]*100,"%)"))+ylab(paste0("PC2: ("explain[1]*100,"%)"))
 
